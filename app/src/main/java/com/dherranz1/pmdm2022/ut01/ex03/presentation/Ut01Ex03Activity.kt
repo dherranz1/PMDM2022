323111e1bf7a91c77992f41d9981eaaf3896eace
dherranz1 <114154511+dherranz1@users.noreply.github.com>
2022-10-04 17:10:00 +0200

    Ex03c - Ex02 done with RelativeLayout and LinearLayout

---
+++ app/src/main/java/com/dherranz1/pmdm2022/ut01/ex03/presentation/Ut01Ex03Activity.kt
@@ -0,0 +1,12 @@
+package com.dherranz1.pmdm2022.ut01.ex03.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dherranz1.pmdm2022.R
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex03)
+    }
+}
---
+++ app/src/main/java/com/dherranz1/pmdm2022/ut01/ex03b/Ut01Ex03bActivity.kt
@@ -0,0 +1,11 @@
+package com.dherranz1.pmdm2022.ut01.ex03b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01Ex03bActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex03b)
+    }
+}
