89dbfb3905a14934158df369bfe528e086f2cda2
dherranz1 <dweshh@proton.me>
2022-10-06 01:36:34 +0200

    Ex05 - Ávila en Tapas Layout

--- app/src/main/java/com/dherranz1/pmdm2022/ut01/ex05Tapas/presentation/Ut01Ex05Activity.kt
+++ app/src/main/java/com/dherranz1/pmdm2022/ut01/ex05Tapas/presentation/Ut01Ex05Activity.kt
@@ -1,12 +1,12 @@
-package com.dherranz1.pmdm2022.ut01.ex03b
+package com.dherranz1.pmdm2022.ut01.ex05Tapas.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dherranz1.pmdm2022.R
 
-class Ut01Ex03bActivity : AppCompatActivity() {
+class Ut01Ex05Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01_ex03b)
+        setContentView(R.layout.activity_ut01_ex05tapas)
     }
 }
